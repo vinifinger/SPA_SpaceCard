d1dfa7208bcf154865085f671f9ff23dc199960d
Gabriel <gabrielf.fritz@gmail.com>
2021-08-31 22:20:05 -0300

    Adicionado botao para exibir password

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,10 +1,19 @@
-import { Box, Checkbox, FormControlLabel, Grid, Link } from '@material-ui/core';
+import { Box, Checkbox, FormControlLabel, Grid, Link, InputAdornment, IconButton } from '@material-ui/core';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import Copyright from '../Copyright';
 import React, { useState } from 'react';
 
 import { StyledAlert, StyledButtonSubmit, StyledForm, StyledTextField } from './styles'; 
 import api from '../../services/api';
 
+interface State {
+    amount: string;
+    password: string;
+    weight: string;
+    weightRange: string;
+    showPassword: boolean;
+  }
 export interface Props {
     error?: string;
 }
@@ -16,6 +25,22 @@ const LoginForm: React.FC<Props> = ({
     const [login, setLogin] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const [values, setValues] = React.useState<State>({
+        amount: '',
+        password: '',
+        weight: '',
+        weightRange: '',
+        showPassword: false,
+      });
+    
+      const handleClickShowPassword = () => {
+        setValues({ ...values, showPassword: !values.showPassword });
+      };
+    
+      const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+      };
 
     async function handleLogin() {
 
@@ -57,11 +82,25 @@ const LoginForm: React.FC<Props> = ({
             fullWidth
             name="password"
             label="Senha"
-            type="password"
+            type={values.showPassword ? 'text' : 'password'}
             id="password"
             autoComplete="current-password"
             value={password}
             onChange={(event) => { setPassword(event.target.value) }}
+            InputProps={{
+                endAdornment:(
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label='toggle password visibility'
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                      >
+                        {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                )
+            }}
+            
             />
             <FormControlLabel
             control={<Checkbox value="remember" color="primary" />}

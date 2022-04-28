29e1be6a3a313a115d67ea284511806e612dceed
vfinger <vinicius.finger@adp.com>
2022-04-28 20:40:47 -0300

    delete component ButtonLoginGoogle

--- src/components/ButtonLoginGoogle/styles.ts
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@material-ui/core";
-import styled from "styled-components";
-
-export const StyledTypography = styled(Typography)`
-    margin-bottom: 20px!important;
-    font-size: 45px!important;
-    font-weight: 900!important;
-    text-align: 'left';
-`;
